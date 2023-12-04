8af6d238012b6a344c2d79c36f2d0a44085870fd
lulone <115679985+lulone@users.noreply.github.com>
2023-12-04 17:53:17 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+def sendDistanceWhenHumanPresence():
+    global distance
+    if Environment.PIR(DigitalPin.P2):
+        distance = sonar.ping(DigitalPin.P10, DigitalPin.P13, PingUnit.CENTIMETERS)
+        serial.write_string("!1:PIR:" + str(distance) + "#")
+    else:
+        serial.write_string("!1:PIR:" + str(0) + "#")
+def sendTempNHumiToGateway():
+    global DHT20_CYCLE
+    DHT20_CYCLE += 1
+    if DHT20_CYCLE == 30:
+        DHT20_CYCLE = 0
+        dht11_dht22.query_data(DHTtype.DHT22, DigitalPin.P3, True, False, False)
+        if dht11_dht22.read_data_successful():
+            serial.write_string("!1:HUMI:" + str(dht11_dht22.read_data(dataType.HUMIDITY)) + "#")
+            basic.pause(100)
+            serial.write_string("!1:TEMP:" + str(dht11_dht22.read_data(dataType.TEMPERATURE)) + "#")
+distance = 0
+DHT20_CYCLE = 0
+basic.show_string("DA")
+DHT20_CYCLE = 0
+
 def on_forever():
-    radio.set_group(1)
+    sendTempNHumiToGateway()
+    sendDistanceWhenHumanPresence()
+    basic.pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+function sendDistanceWhenHumanPresence () {
+    if (Environment.PIR(DigitalPin.P2)) {
+        distance = sonar.ping(
+        DigitalPin.P10,
+        DigitalPin.P13,
+        PingUnit.Centimeters
+        )
+        serial.writeString("!1:PIR:" + distance + "#")
+    } else {
+        serial.writeString("!1:PIR:" + 0 + "#")
+    }
+}
+function sendTempNHumiToGateway () {
+    DHT20_CYCLE += 1
+    if (DHT20_CYCLE == 30) {
+        DHT20_CYCLE = 0
+        dht11_dht22.queryData(
+        DHTtype.DHT22,
+        DigitalPin.P3,
+        true,
+        false,
+        false
+        )
+        if (dht11_dht22.readDataSuccessful()) {
+            serial.writeString("!1:HUMI:" + dht11_dht22.readData(dataType.humidity) + "#")
+            basic.pause(100)
+            serial.writeString("!1:TEMP:" + dht11_dht22.readData(dataType.temperature) + "#")
+        }
+    }
+}
+let distance = 0
+let DHT20_CYCLE = 0
+basic.showString("DA")
+DHT20_CYCLE = 0
 basic.forever(function () {
-    radio.setGroup(1)
+    sendTempNHumiToGateway()
+    sendDistanceWhenHumanPresence()
+    basic.pause(1000)
 })
